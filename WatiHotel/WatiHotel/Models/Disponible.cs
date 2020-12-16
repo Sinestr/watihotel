1fba1b1317b9bacc41e9326d40f7028df6de4642
Tangi GREFFIER <tangigreffier@gmail.com>
2020-12-16 11:11:41 +0100

    add dispo class

---
+++ WatiHotel/WatiHotel/Models/Disponible.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WatiHotel.Models
+{
+    public class Disponible
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private int _Room_available;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private DateTime _Date_Room;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Room_available { get => _Room_available; set => _Room_available = value; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Date_Room { get => _Date_Room; set => _Date_Room = value; }
+
+
+
+    }
+}
