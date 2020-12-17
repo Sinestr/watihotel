3a6f1e5254a64f0cde34b2d71ba42c91861a818c
Tangi GREFFIER <tangigreffier@gmail.com>
2020-12-17 10:13:55 +0100

    360 No scope titanic

---
+++ WatiAgreg/WatiAgreg/Controller/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WatiAgreg.Controller
+{
+    public class ValuesController : ApiController
+    {
+
+    }
+}
