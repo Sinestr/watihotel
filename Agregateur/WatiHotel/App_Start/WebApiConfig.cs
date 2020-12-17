4d1a226f5c257af8dadf96917b6b39c1ec4ca6fd
Tangi GREFFIER <tangigreffier@gmail.com>
2020-12-17 15:09:16 +0100

    Renew Agregateur project

---
+++ Agregateur/WatiHotel/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using WatiHotel.Models;
+
+namespace WatiHotel
+{
+    public static class WebApiConfig
+    {
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+
+
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(
+            config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml"));
+
+        }
+    }
+}
---
+++ Agregateur/WatiHotel/Controller/HotelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System.IO;
+
+namespace WatiAgreg.Controller
+{
+    [Route("watiAgrega/Hotels")]
+    public class HotelsController : ApiController
+    {
+       
+    }
+}
