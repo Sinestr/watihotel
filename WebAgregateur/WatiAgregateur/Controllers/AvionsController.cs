affa5bb9666e1cb848c1a7c425f02d5c9feb24cf
Tangi GREFFIER <tangigreffier@gmail.com>
2020-12-17 15:54:50 +0100

    Renew Agregateur

---
+++ WebAgregateur/WatiAgregateur/Controllers/AvionsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace WatiAgregateur.Controllers
+{
+    [Route("watiAgrega/Avions")]
+    public class AvionsController : ApiController
+    {
+        [Route("GetAvions")]
+        [HttpGet]
+        public string GetAvions()
+        {
+            //Préparation de la requête
+            string method = "GET";
+            string query = "https://trivaphp.herokuapp.com/api/planes";
+            //Création de la requête
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(query);
+            req.Method = method;
+            req.ContentType = "application/json";
+            req.Headers.Add("Api-token:" + "ZPDpXWyKDeavzEDXtMHip89eGN9gSuRzasoDrTc9vKo27YIxJ9");
+            //Appel de la requête
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)req.GetResponse();
+
+            var repStream = myHttpWebResponse.GetResponseStream();
+            var reader = new StreamReader(repStream);
+            return reader.ReadToEnd();
+        }
+
+        [Route("Get")]
+        [HttpGet]
+        public string Get()
+        {
+            return "toto et si c'est déjà prit, return titi";
+        }
+
+    }
+}
---
+++ WebAgregateur/WatiAgregateur/Models/AvionMP.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WatiAgregateur.Models
+{
+    [DataContract]
+    [JsonObject(MemberSerialization.OptOut)]
+    public class AvionMP
+    {
+        #region Fields
+        private int id;
+        private string name;
+        private DateTime created_at;
+        private DateTime updated_at;
+        #endregion
+
+        #region Constructor
+        public AvionMP() { }
+        #endregion
+
+        #region Properties
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public DateTime Created_at { get => created_at; set => created_at = value; }
+        public DateTime Updated_at { get => updated_at; set => updated_at = value; }
+        #endregion
+    }
+}
